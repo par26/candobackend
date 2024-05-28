8750e30f526b34b6936799bd93a8389056fda958
rocketmind26 <apratush26@gmail.com>
2024-05-27 19:47:55 -0700

    added image to report generation

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -7,11 +7,17 @@ const catchAsync = require("../utils/catchAsync");
 const Company = require("../models/companyModel");
 const { Readable } = require("stream");
 
+
+
 const compile = async function (templateName, data) {
+    
     const filePath = path.join(process.cwd(), "public", `${templateName}.hbs`);
     const html = await fs.readFileSync(filePath, "utf8");
+
     return hbs.compile(html)(data);
 };
+
+
 
 exports.generatePdf = catchAsync(async (req, res, next) => {
     const companies = await Company.find({ owner: req.user._id });
@@ -20,11 +26,15 @@ exports.generatePdf = catchAsync(async (req, res, next) => {
 
     const userName = req.user.firstName;
 
+    const tags = await getTags(companies.map(company => company.toObject()));
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     const content = await compile("reportTemplate", {
         // convert companies to js object
+        data:  Array.from(tags.keys()),
+        labels:  Array.from(tags.values()),
         companies: companies.map(company => company.toObject()),
         ...req.body,
     });
@@ -44,3 +54,24 @@ exports.generatePdf = catchAsync(async (req, res, next) => {
     res.setHeader("Content-Disposition", "attachment; filename=quote.pdf");
     stream.pipe(res);
 });
+
+
+const getTags = async function(companyData) {
+    let tags = new Map();
+
+    for(let company in companyData) {
+        for(let key in company.type) {
+        
+            let companyTag = company.type[key];
+    
+            if(tags.get(companyTag) == undefined) {
+                tags.set(companyTag, 1);
+            } else {
+                tags.set(companyTag, tags.get(companyTag)+1);
+            }
+        }
+    
+    }
+    
+    return tags;
+};
