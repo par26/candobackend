2e74d95a0acbf697da0730beea55060e9ca6459a
Lucas Fan <xfan0126@gmail.com>
2024-05-29 16:57:20 -0700

    basic bar graph for most common tags

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -7,35 +7,32 @@ const catchAsync = require("../utils/catchAsync");
 const Company = require("../models/companyModel");
 const { Readable } = require("stream");
 
-
+const NUM_TAGS_TO_DISPLAY = 7;
 
 const compile = async function (templateName, data) {
-    
     const filePath = path.join(process.cwd(), "public", `${templateName}.hbs`);
     const html = await fs.readFileSync(filePath, "utf8");
 
     return hbs.compile(html)(data);
 };
 
-
-
 exports.generatePdf = catchAsync(async (req, res, next) => {
     const companies = await Company.find({ owner: req.user._id });
+    const companiesArray = companies.map(company => company.toObject());
     const body = req.body;
-    console.log(body);
 
     const userName = req.user.firstName;
 
-    const tags = await getTags(companies.map(company => company.toObject()));
+    const tags = await getCommonTags(companiesArray);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     const content = await compile("reportTemplate", {
         // convert companies to js object
-        data:  Array.from(tags.keys()),
-        labels:  Array.from(tags.values()),
-        companies: companies.map(company => company.toObject()),
+        data: Array.from(tags.keys()),
+        labels: Array.from(tags.values()),
+        companies: companiesArray,
         ...req.body,
     });
 
@@ -55,23 +52,22 @@ exports.generatePdf = catchAsync(async (req, res, next) => {
     stream.pipe(res);
 });
 
-
-const getTags = async function(companyData) {
+const getCommonTags = async function (companyData) {
     let tags = new Map();
 
-    for(let company in companyData) {
-        for(let key in company.type) {
-        
-            let companyTag = company.type[key];
-    
-            if(tags.get(companyTag) == undefined) {
-                tags.set(companyTag, 1);
+    for (const company of companyData) {
+        for (const tag of company.type) {
+            if (tags.get(tag) == undefined) {
+                tags.set(tag, 1);
             } else {
-                tags.set(companyTag, tags.get(companyTag)+1);
+                tags.set(tag, tags.get(tag) + 1);
             }
         }
-    
     }
-    
-    return tags;
+
+    const tagsArray = Array.from(tags.entries());
+    tagsArray.sort((a, b) => b[1] - a[1]);
+    const top5Tags = new Map(tagsArray.slice(0, NUM_TAGS_TO_DISPLAY));
+
+    return top5Tags;
 };
